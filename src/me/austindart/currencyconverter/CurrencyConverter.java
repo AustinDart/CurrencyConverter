ce50c9bb7c950d7b4024f83dc50f257f3340d9cb
AustinDart <austindart02@gmail.com>
2022-10-26 15:42:06 -0500

    Clean up code

--- src/me/austindart/currencyconverter/CurrencyConverter.java
+++ src/me/austindart/currencyconverter/CurrencyConverter.java
@@ -1,16 +1,11 @@
 package me.austindart.currencyconverter;
 
 import me.austindart.currencyconverter.gui.ConverterGUI;
-import me.austindart.currencyconverter.utils.ConversionUtils;
-import org.json.simple.parser.ParseException;
-
-import java.io.IOException;
 
 public class CurrencyConverter
 {
 
-    public static void main(String[] args) throws IOException, ParseException
-    {
+    public static void main(String[] args) {
         new ConverterGUI();
     }
 
