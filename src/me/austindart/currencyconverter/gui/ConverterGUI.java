9183c7e1ed395523ef39e78097d33b48c95be563
AustinDart <austindart02@gmail.com>
2022-10-26 16:15:38 -0500

    Add support for all currencies supported by currencylayer

--- src/me/austindart/currencyconverter/gui/ConverterGUI.java
+++ src/me/austindart/currencyconverter/gui/ConverterGUI.java
@@ -6,8 +6,7 @@ import me.austindart.currencyconverter.gui.buttonlisteners.ConvertButtonListener
 import javax.swing.*;
 import java.awt.*;
 
-public class ConverterGUI extends BaseGUI
-{
+public class ConverterGUI extends BaseGUI {
 
     private String apiKey;
 
@@ -26,15 +25,26 @@ public class ConverterGUI extends BaseGUI
     private JTextField apiKeyTextField;
     private JButton apiKeySubmitButton;
 
-    public ConverterGUI()
-    {
+    public ConverterGUI() {
         super();
         inputLabel = new JLabel("Input amount:");
         inputTextField = new JTextField(10);
         outputLabel = new JLabel("Output amount:");
         outputText = new JLabel("0");
 
-        String[] supportedCurrencies = {"USD", "EUR"};
+        String[] supportedCurrencies = {"FJD", "MXN", "STD", "LVL", "SCR", "CDF", "BBD", "GTQ", "CLP", "HNL", "UGX",
+                "ZAR", "TND", "CUC", "BSD", "SLL", "SDG", "IQD", "CUP", "GMD", "TWD", "RSD", "DOP", "KMF", "MYR", "FKP",
+                "XOF", "GEL", "BTC", "UYU", "MAD", "CVE", "TOP", "AZN", "OMR", "PGK", "KES", "SEK", "BTN", "UAH", "GNF",
+                "ERN", "MZN", "SVC", "ARS", "QAR", "IRR", "MRO", "CNY", "THB", "UZS", "XPF", "BDT", "LYD", "BMD", "KWD",
+                "PHP", "RUB", "PYG", "ISK", "JMD", "COP", "MKD", "USD", "DZD", "PAB", "GGP", "SGD", "ETB", "JEP", "KGS",
+                "SOS", "VEF", "VUV", "LAK", "BND", "ZMK", "XAF", "LRD", "XAG", "CHF", "HRK", "ALL", "DJF", "ZMW", "TZS",
+                "VND", "XAU", "AUD", "ILS", "GHS", "GYD", "KPW", "BOB", "KHR", "MDL", "IDR", "KYD", "AMD", "BWP", "SHP",
+                "TRY", "LBP", "TJS", "JOD", "AED", "HKD", "RWF", "EUR", "LSL", "DKK", "CAD", "BGN", "MMK", "MUR", "NOK",
+                "SYP", "IMP", "ZWL", "GIP", "RON", "LKR", "NGN", "CRC", "CZK", "PKR", "XCD", "ANG", "HTG", "BHD", "KZT",
+                "SRD", "SZL", "LTL", "SAR", "TTD", "YER", "MVR", "AFN", "INR", "AWG", "KRW", "NPR", "JPY", "MNT", "AOA",
+                "PLN", "GBP", "SBD", "BYN", "HUF", "BYR", "BIF", "MWK", "MGA", "XDR", "BZD", "BAM", "EGP", "MOP", "NAD",
+                "NIO", "PEN", "NZD", "WST", "TMT", "CLF", "BRL"};
+
         SpinnerListModel curModel = new SpinnerListModel(supportedCurrencies);
         inputSpinner = new JSpinner(curModel);
         SpinnerListModel curModel2 = new SpinnerListModel(supportedCurrencies);
@@ -71,53 +81,42 @@ public class ConverterGUI extends BaseGUI
         jFrame.pack();
     }
 
-    public String getInputCurrency()
-    {
+    public String getInputCurrency() {
         return inputSpinner.getValue().toString();
     }
 
-    public String getOutputCurrency()
-    {
+    public String getOutputCurrency() {
         return outputSpinner.getValue().toString();
     }
 
-    public double getValueFromInput()
-    {
+    public double getValueFromInput() {
         String inputText = inputTextField.getText();
         double value;
-        try
-        {
+        try {
             value = Double.parseDouble(inputText);
-        }
-        catch (NumberFormatException e)
-        {
+        } catch (NumberFormatException e) {
             value = -1;
         }
         return value;
     }
 
-    public void setOutputText(String text)
-    {
+    public void setOutputText(String text) {
         outputText.setText(text);
     }
 
-    public void setAPIKey(String apiKey)
-    {
+    public void setAPIKey(String apiKey) {
         this.apiKey = apiKey;
     }
 
-    public String getAPIKey()
-    {
+    public String getAPIKey() {
         return apiKey;
     }
 
-    public String getInputAPIKey()
-    {
+    public String getInputAPIKey() {
         return apiKeyTextField.getText();
     }
 
-    private void setupButtons()
-    {
+    private void setupButtons() {
         calculateButton.addActionListener(new ConvertButtonListener(this));
         apiKeySubmitButton.addActionListener(new APIKeySubmitButtonListener(this));
     }
